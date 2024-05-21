abc3e32a3e4635a93aca87ec204e5a9fc0c57678
Richard Wells <Richard.Wells@normandale.edu>
2024-05-21 16:20:33 -0500

    Added documentation.

--- Dijkstra/CreateData/point.py
+++ Dijkstra/CreateData/point.py
@@ -1,13 +1,19 @@
 '''
     point.py
-
-    A simple Point data type with x and y coordinates.
 '''
 
 import math
 
 class Point:
-    ''' The Point class '''
+    ''' The Point class
+
+    This Point class represents a 2-dimensional
+    point with integer coordinates.
+
+    Since this class has no invariant, the
+    x and y values are publicly available and
+    may be modified by client code.
+    '''
 
     def __init__(self, x: int, y: int):
         self.x: int = x
@@ -16,6 +22,10 @@ class Point:
 
     def __str__(self):
         return f"({self.x},{self.y})"
+
+
+    def __repr__(self):
+        return f"Point({self.x},{self.y})"
 
 
     def __hash__(self):
@@ -27,19 +37,31 @@ class Point:
 
 
     def distance_to(self, pt: 'Point') -> float:
-        ''' Compute the distance from self to pt'''
+        ''' Compute the distance from self to pt
+
+        The distance is returned as a floating
+        point number.
+        '''
         dx = pt.x - self.x
         dy = pt.y - self.y
         return math.sqrt(dx*dx + dy*dy)
 
 
 def distance_between(pt1: Point, pt2: Point):
-    ''' Computer the distance between two points '''
+    ''' Compute the distance between two points
+
+        The distance is returned as a floating
+        point number.
+    '''
     return pt1.distance_to(pt2)
 
 
 def parse_point(s):
-    ''' Convert a string to a Point'''
+    ''' Convert a string to a Point
+
+    The string must be in the format produced
+    by __str__.
+    '''
     if s[0] != "(":
         raise ValueError("parse_point: does not start with '('")
     if s[-1] != ")":
